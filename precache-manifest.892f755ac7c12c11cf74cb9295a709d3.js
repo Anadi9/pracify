d740e865b1b86b0537a1bb6fce97da055c1b143c
Anadi9 <anadithakur99@gmail.com>
2020-09-08 22:17:55 +0530

    Updates

---
+++ precache-manifest.892f755ac7c12c11cf74cb9295a709d3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed551a8fd081f882006081f5750e204c",
+    "url": "/pracify/index.html"
+  },
+  {
+    "revision": "b827f2bbab0cf27d9181",
+    "url": "/pracify/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "585498171ee8178e7868",
+    "url": "/pracify/static/css/main.37f76905.chunk.css"
+  },
+  {
+    "revision": "b827f2bbab0cf27d9181",
+    "url": "/pracify/static/js/2.d7341080.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pracify/static/js/2.d7341080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585498171ee8178e7868",
+    "url": "/pracify/static/js/main.055d32d6.chunk.js"
+  },
+  {
+    "revision": "f940a572f1415a08d3bb",
+    "url": "/pracify/static/js/runtime-main.05aed5ce.js"
+  },
+  {
+    "revision": "8ce59080098b2bc8f735d1103476e519",
+    "url": "/pracify/static/media/Group 800.8ce59080.svg"
+  },
+  {
+    "revision": "25e58dee1e45e82db3bcf6a619fa199d",
+    "url": "/pracify/static/media/Group 801.25e58dee.svg"
+  },
+  {
+    "revision": "93641ad4c5c56b86b18f853904b3c2b1",
+    "url": "/pracify/static/media/Icon awesome-heart.93641ad4.svg"
+  },
+  {
+    "revision": "f514385ff824dadb4cf7d96760f25b82",
+    "url": "/pracify/static/media/google-play-badge@3x.f514385f.png"
+  },
+  {
+    "revision": "cd2181b256dc70f4b2d20f58e4296ebd",
+    "url": "/pracify/static/media/handshake.cd2181b2.svg"
+  },
+  {
+    "revision": "d87b127534cc2d9da99445cad512ac53",
+    "url": "/pracify/static/media/rupee.d87b1275.svg"
+  },
+  {
+    "revision": "45a9831dd299872439c0c409296280bf",
+    "url": "/pracify/static/media/surface1.45a9831d.svg"
+  }
+]);
